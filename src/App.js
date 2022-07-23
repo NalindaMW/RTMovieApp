a0b7485c5dcd8f87f9a62581866bb4be7631a789
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-07-23 09:52:48 +0530

    Handling loading state and error state

--- src/App.js
+++ src/App.js
@@ -6,21 +6,29 @@ import axios from "axios";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   async function fetchMoviesHandler() {
-    const response = await axios.get(
-      "https://swapi.py4e.com/api/films/?format=json"
-    );
-    const movieResult = response.data.results;
-    const transformedMovies = movieResult.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    setIsLoading(true);
+    try {
+      const response = await axios.get(
+        "https://swapi.py4e.com/api/films/?format=json"
+      );
+      const movieResult = response.data.results;
+      const transformedMovies = movieResult.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(true);
+    }
+    setIsLoading(false);
   }
 
   return (
@@ -29,7 +37,10 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && movies.length === 0 && <p>No Movies Found.</p>}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {error && <p>Something went wrong!</p>}
       </section>
     </React.Fragment>
   );
